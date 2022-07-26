1b88e3a3160fc75b8353a7ca7eefa14cfcefaca9
Sultepronit <sultepronit@gmail.com>
2022-07-26 18:07:27 +0300

    Studied printf

--- String-Formatting/src/App.java
+++ String-Formatting/src/App.java
@@ -7,5 +7,18 @@ public class App {
        for(int i = 0; i < 20; i++) {
     	   System.out.printf("%2d: some text here\n", i);
        }
+       
+       System.out.printf("%2d: %s\n", 33, "here is some text");
+       
+       float number = 5.6f;
+       double dNumber = 5.6;
+       System.out.println(number);
+       System.out.println(dNumber);
+       System.out.printf("Some number %f\n", number);
+       System.out.printf("Some number %f\n", dNumber);
+       System.out.printf("Some number %.2f\n", number);
+       System.out.printf("Some number %.2f\n", dNumber);
+       
+       System.out.printf("Some number '%7.2f'\n", 32.578);
     }
 }
